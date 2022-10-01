4497ee4ab15439d2191229173c5b2c5abc021c1b
amar-nath1 <amarat279955>
2022-10-01 16:34:17 +0530

    changes in list items

--- domp.js
+++ domp.js
@@ -1,2 +1,8 @@
-var a=document.getElementsByClassName('list-group')
-console.log(a)
+var listitem=document.getElementsByClassName('list-group-item')
+listitem[2].style.background='lightgreen'
+listitem[2].style.border='2px solid lightcoral'
+
+for (let i=0;i<listitem.length;i++){
+    listitem[i].style.fontWeight="bold"
+    listitem[i].style.color="tomato"
+}
